466107977a8f73de0fe0981f164a12ccab5fc5c2
Dmitrii Fokin <fokinds1@gmail.com>
2021-01-13 13:41:06 +0300

    first commit. Init empty project

---
+++ src/main/kotlin/ru/fdo/bamk/finrez/employeeservicecommon/EmployeeServiceCommonApplication.kt
@@ -0,0 +1,11 @@
+package ru.fdo.bamk.finrez.employeeservicecommon
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EmployeeServiceCommonApplication
+
+fun main(args: Array<String>) {
+    runApplication<EmployeeServiceCommonApplication>(*args)
+}
---
+++ src/test/kotlin/ru/fdo/bamk/finrez/employeeservicecommon/EmployeeServiceCommonApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.fdo.bamk.finrez.employeeservicecommon
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EmployeeServiceCommonApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
