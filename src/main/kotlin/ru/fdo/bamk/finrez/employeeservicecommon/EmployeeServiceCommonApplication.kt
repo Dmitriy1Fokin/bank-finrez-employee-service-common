9f212741d100abfd2a6f7dde6715e08a6bb166ab
Dmitrii Fokin <fokinds1@gmail.com>
2021-01-13 14:14:33 +0300

    fix main class

--- src/main/kotlin/ru/fdo/bamk/finrez/employeeservicecommon/EmployeeServiceCommonApplication.kt
+++ src/main/kotlin/ru/fdo/bamk/finrez/employeeservicecommon/EmployeeServiceCommonApplication.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class EmployeeServiceCommonApplication
+open class EmployeeServiceCommonApplication
 
 fun main(args: Array<String>) {
     runApplication<EmployeeServiceCommonApplication>(*args)
